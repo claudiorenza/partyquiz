351d059d58f131dfd2332bc84bb9697e3b612691
Claudio Renza <kloudpix@gmail.com>
2018-02-14 15:08:31 +0100

    UIButton Extension

---
+++ frontend/SimpleQuestion4Claudio/SimpleQuestion4Claudio/UIButtonExtension.swift
@@ -0,0 +1,41 @@
+//
+//  UIButtonExtension.swift
+//  SimpleQuestion4Claudio
+//
+//  Created by Claudio Renza on 14/02/2018.
+//  Copyright © 2018 Giovanni Frate. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIButton  {
+  
+  func entering(directionFrom: String, view: UIView)  {
+    let enterAnimation = CABasicAnimation(keyPath: "position")
+    
+    
+    var cgPointButtonInit = self.center
+    let cgPointButtonFinal = cgPointButtonInit
+    
+    if(directionFrom == "left") {
+      cgPointButtonInit.x = -self.frame.width
+    } else if (directionFrom == "right")    {
+      cgPointButtonInit.x = view.bounds.width + self.frame.width
+      
+    }
+    
+    
+    enterAnimation.fromValue = NSValue(cgPoint: cgPointButtonInit)
+    enterAnimation.toValue = NSValue(cgPoint: cgPointButtonFinal)
+    
+    
+    enterAnimation.duration = 1.0
+    enterAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+    
+    layer.add(enterAnimation, forKey: nil)
+  }
+  
+  
+}
+
