f6b67309889afeacd433a5bf4b59aaf34483d741
Claudio Renza <kloudpix@gmail.com>
2018-02-14 14:01:58 +0100

    Xib Buzzer

--- frontend/CoreAnimation/CoreAnimation/Controller/BuzzerViewController.swift
+++ frontend/CoreAnimation/CoreAnimation/Controller/BuzzerViewController.swift
@@ -20,19 +20,48 @@ class BuzzerViewController: UIViewController {
 
         if let questionView = Bundle.main.loadNibNamed("QuestionView", owner: self, options: nil)?.first as? QuestionView {
           
-          questionView.frame = CGRect(x: 10, y: 0, width: 300, height: 300)
-          questionView.center = self.view.center
+          questionView.frame = CGRect(x: 20, y: 32, width: 256, height: 256)
+          
+          /*
+          questionView.translatesAutoresizingMaskIntoConstraints = false
+          questionView.addConstraint(NSLayoutConstraint(item: self.questionView,
+                                                          attribute: NSLayoutAttribute.height,
+                                                          relatedBy: NSLayoutRelation.equal,
+                                                          toItem: self.questionView,
+                                                          attribute: NSLayoutAttribute.width,
+                                                          multiplier: self.questionView.frame.size.height / self.questionView.frame.size.width,
+                                                          constant: 0))
+          //questionView.center = self.view.center
+ 
+          */
           self.view.addSubview(questionView)
           
           
         }
-      
-      
-      
+      /*
+        if let answersView = Bundle.main.loadNibNamed("AnswersView", owner: self, options: nil)?.first as? AnswersView {
+          
+          answersView.frame = CGRect(x: 292, y:32, width: 256, height: 256)
+          
+          self.view.addSubview(answersView)
+          
+          
+        }
+      */
         if let classicBuzzerView = Bundle.main.loadNibNamed("ClassicBuzzerView", owner: self, options: nil)?.first as? ClassicBuzzerView {
           
-          classicBuzzerView.frame = CGRect(x: 200, y: 0, width: 300, height: 300)
-          classicBuzzerView.center = self.view.center
+          classicBuzzerView.frame = CGRect(x: 292, y:32, width: 256, height: 256)
+          /*
+          classicBuzzerView.translatesAutoresizingMaskIntoConstraints = false
+          classicBuzzerView.addConstraint(NSLayoutConstraint(item: self.classicBuzzerView,
+                                                        attribute: NSLayoutAttribute.height,
+                                                        relatedBy: NSLayoutRelation.equal,
+                                                        toItem: self.questionView,
+                                                        attribute: NSLayoutAttribute.width,
+                                                        multiplier: self.classicBuzzerView.frame.size.height / self.classicBuzzerView.frame.size.width,
+                                                        constant: 0))
+          //classicBuzzerView.center = self.view.center
+           */
           self.view.addSubview(classicBuzzerView)
           
           
