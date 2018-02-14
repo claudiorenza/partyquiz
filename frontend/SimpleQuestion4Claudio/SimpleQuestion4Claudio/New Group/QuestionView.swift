dd905320e45d7f75d8cc60294342863ddda623ff
Claudio Renza <kloudpix@gmail.com>
2018-02-14 15:42:34 +0100

    Xib Buzzer

---
+++ frontend/SimpleQuestion4Claudio/SimpleQuestion4Claudio/New Group/QuestionView.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionView.swift
+//  CoreAnimation
+//
+//  Created by Claudio Renza on 13/02/2018.
+//  Copyright © 2018 Abusive Designers. All rights reserved.
+//
+
+import UIKit
+
+class QuestionView: UIView {
+  
+  @IBOutlet var labelQuestion: UILabel!
+  
+  
+}
+
