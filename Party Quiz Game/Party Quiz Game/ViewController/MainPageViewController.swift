a0df0143aec1eb332a143c401d34337048bbb9c1
Claudio Renza <kloudpix@gmail.com>
2018-02-19 17:57:49 +0100

    Storyboard

---
+++ Party Quiz Game/Party Quiz Game/ViewController/MainPageViewController.swift
@@ -0,0 +1,76 @@
+//
+//  MainPageViewController.swift
+//  SimpleQuestion4Claudio
+//
+//  Created by Giovanni Frate on 14/02/18.
+//  Copyright © 2018 Giovanni Frate. All rights reserved.
+//
+
+import UIKit
+import MultipeerConnectivity
+import AVFoundation
+
+class MainPageViewController: UIViewController {
+  
+  @IBOutlet weak var createGameOutlet: UIButton!
+  @IBOutlet weak var joinGameOutlet: UIButton!
+  @IBOutlet weak var labelOutlet: UILabel!
+  
+  var audioPlayerButtonClick = AVAudioPlayer()
+  var audioPlayerMusic = AVAudioPlayer()
+  let borderColor = UIColor(red: 96.0/255.0, green: 96.0/255.0, blue: 96.0/255.0, alpha: 1.0).cgColor
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setButton(tempButton: createGameOutlet)
+    setButton(tempButton: joinGameOutlet)
+    
+    let audioButtonClick = Bundle.main.path(forResource: "buttonClick", ofType: "m4a")
+    let audioIntroMusic = Bundle.main.path(forResource: "musicIntro", ofType: "m4a")
+    
+    
+    // copy this syntax, it tells the compiler what to do when action is received
+    do {
+      audioPlayerButtonClick = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioButtonClick! ))
+      audioPlayerMusic = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioIntroMusic! ))
+      try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryAmbient)
+      try AVAudioSession.sharedInstance().setActive(true)
+    }
+    catch{
+      print(error)
+    }
+    
+    audioPlayerMusic.play()
+    
+    createGameOutlet.entering(directionFrom: "left", view: self.view)
+    joinGameOutlet.entering(directionFrom: "right", view: self.view)
+  }
+  
+  
+  func setButton(tempButton: UIButton) {
+    tempButton.layer.cornerRadius = 25.0
+    tempButton.layer.borderColor = borderColor
+    tempButton.layer.borderWidth = 8.0
+  }
+  
+  @IBAction func pressToCreate(_ sender: UIButton) {
+//    PeerManager.shared.stopBrowser()
+//    PeerManager.shared.setupAdvertise()
+//    PeerManager.shared.startAdvertise()
+//    present(PeerManager.shared.controller, animated: true, completion: nil)
+    
+    audioPlayerButtonClick.play()
+  }
+  
+  @IBAction func pressToJoin(_ sender: UIButton) {
+//    PeerManager.shared.stopAdvertise()
+//    PeerManager.shared.setupSession()
+//    PeerManager.shared.setupBrowser()
+//    present(PeerManager.shared.controller, animated: true, completion: nil)
+    audioPlayerButtonClick.play()
+  }
+  
+  @IBAction func pressForInfo(_ sender: UIButton) {
+    labelOutlet.text = "Infos..."
+  }
+}
