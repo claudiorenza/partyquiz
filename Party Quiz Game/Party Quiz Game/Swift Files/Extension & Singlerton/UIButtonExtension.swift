6dc6fcf72f5b43d7a629df53fb7161a52b5d7fd9
Claudio Renza <kloudpix@gmail.com>
2018-02-23 14:38:04 +0100

    Quizzer Logo Animation

--- Party Quiz Game/Party Quiz Game/Swift Files/Extension & Singlerton/UIButtonExtension.swift
+++ Party Quiz Game/Party Quiz Game/Swift Files/Extension & Singlerton/UIButtonExtension.swift
@@ -11,7 +11,7 @@ import UIKit
 
 extension UIButton  {
   
-  func entering(directionFrom: String, view: UIView) {
+  func entering(directionFrom: String, view: UIView, duration: Double) {
     let animation = CABasicAnimation(keyPath: "position")
     var cgPointButtonInit = self.center
     let cgPointButtonFinal = cgPointButtonInit
@@ -24,13 +24,13 @@ extension UIButton  {
     
     animation.fromValue = NSValue(cgPoint: cgPointButtonInit)
     animation.toValue = NSValue(cgPoint: cgPointButtonFinal)
-    animation.duration = 1.0
+    animation.duration = duration
     animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
     
     layer.add(animation, forKey: nil)
   }
   
-  func exit(directionTo: String, view: UIView) {
+  func exit(directionTo: String, view: UIView, duration: Double) {
     let animation = CABasicAnimation(keyPath: "position")
     var finalPosition = CGPoint()
     
@@ -42,7 +42,7 @@ extension UIButton  {
     
     animation.fromValue = self.center
     animation.toValue = CGPoint(x: finalPosition.x, y: self.center.y)
-    animation.duration = 1.0
+    animation.duration = duration
     animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
     
     layer.add(animation, forKey: nil)
